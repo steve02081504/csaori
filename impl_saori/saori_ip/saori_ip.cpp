b26a60d9c0c8f1e3f421b0179a8d40e079b5f30a
steve02081504 <steve02081504@EDEN.fukwold>
2021-07-04 14:10:55 +0800

    Update saori_ip.cpp

--- impl_saori/saori_ip/saori_ip.cpp
+++ impl_saori/saori_ip/saori_ip.cpp
@@ -27,19 +27,19 @@ bool CSAORI::unload()
 void CSAORI::exec(const CSAORIInput& in,CSAORIOutput& out)
 {
 	const string_t& cmd = in.args[0];
-	if (wcsnicmp(cmd.c_str(), L"GetLocalIP", 10) == 0) {
+	if (_wcsnicmp(cmd.c_str(), L"GetLocalIP", 10) == 0) {
 		if (GetLocalIP(out.result)) {
 			out.result_code = SAORIRESULT_OK;
 		}else
 			out.result_code = SAORIRESULT_INTERNAL_SERVER_ERROR;
 		return;
-	} else if (wcsnicmp(cmd.c_str(), L"GetInternetIP", 13) == 0) {
+	} else if (_wcsnicmp(cmd.c_str(), L"GetInternetIP", 13) == 0) {
 		if (GetInternetIP(out.result)) {
 			out.result_code = SAORIRESULT_OK;
 		}else
 			out.result_code = SAORIRESULT_INTERNAL_SERVER_ERROR;
 		return;
-	} else if (wcsnicmp(cmd.c_str(), L"GetLocalHostname", 16) == 0) {
+	} else if (_wcsnicmp(cmd.c_str(), L"GetLocalHostname", 16) == 0) {
 		if (GetLocalHostname(out.result)) {
 			out.result_code = SAORIRESULT_OK;
 		} else
